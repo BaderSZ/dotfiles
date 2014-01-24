24e1e46ce0a27ea70e3afbe03abc949f3a97d235
Bader Zaidan <bader.zaidan@gmail.com>
2014-01-24 16:18:53 +0100

    Delete sunny.sh

--- .scripts/sunny.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-DIRECTORY="$HOME/media/TV/It's Always Sunny"
-EXTENSION="avi"
-FILE=$(cd "$DIRECTORY"; find * | grep ${EXTENSION}$ | shuf -n 1)
-PATH="$DIRECTORY/$FILE"
-/usr/bin/vlc "$PATH"
